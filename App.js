35e00202f8c1768816e7f03979f2b330b61e402f
Reaper8686 <pratikbangera72@gmail.com>
2022-06-17 17:53:04 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.sectionContainer}>
+      <Text>Hello App</Text>
+      <Text>Hello App</Text>
+      <Text>Hello App</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
